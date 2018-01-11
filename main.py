26ec5314630540f0f3f1c2ca3fcc1efa9cedcb78
Soo Jung Bae <bae@Soos-MacBook-Pro.local>
2018-01-11 01:23:46 -0500

    Uploaded all menu to database and connected it to menu page

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
-from data import Appetizers, Lunch, Tray, SoupNSalad, Maki, VegMaki, Maki2
-# from flask_mysqldb import MySQL
-# from wtforms import Form, StringField, IntegerField
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, IntegerField
 # from sassutils.wsgi import SassMiddleware
 
 app = Flask(__name__)
 app.debug = True
 
-# mysql = MySQL()
-# app.config['MYSQL_DATABASE_USER'] = 'root'
-# app.config['MYSQL_DATABASE_PASSWORD'] = 'tnWJD123~!'
-# app.config['MYSQL_DATABASE_DB'] = 'niji'
-# app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-# mysql.init_app(app)
+mysql = MySQL()
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'tnWJD123~!'
+app.config['MYSQL_DB'] = 'niji'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor' #from tuple to dictionary
+mysql.init_app(app)
+
 # app.wsgi_app = SassMiddleware(app.wsgi_app, {
 #     'myapp': ('static/sass', 'static/css', '/static/css')
 # })
-
-Appetizers = Appetizers()
-Lunch = Lunch()
-Tray = Tray()
-SoupNSalad = SoupNSalad()
-Maki = Maki()
-VegMaki = VegMaki()
-Maki2 = Maki2()
 
 @app.route('/form')
 def form():
@@ -49,6 +42,57 @@ def home():
 
 @app.route('/menu')
 def menu():
+    cur = mysql.connection.cursor()
+    result = cur.execute('select distinct m_id from options')
+    result2 = cur.fetchall()
+    optionsArray = []
+    for o in result2:
+        optionsArray.append(o['m_id'])
 
-    return render_template('menu.html', appetizers=Appetizers, lunch=Lunch, 
-        tray=Tray, soupNsalad=SoupNSalad, maki=Maki, vegMaki=VegMaki, maki2=Maki2)
+    result = cur.execute('select * from options')
+    options = cur.fetchall()
+
+    result = cur.execute('select * from menu where type="lunch"')
+    lunch = cur.fetchall()
+
+    result = cur.execute('select * from menu where type="appetizer"')
+    appetizers = cur.fetchall()
+
+    result = cur.execute('select * from menu where type="tray"')
+    tray = cur.fetchall()
+
+    result = cur.execute('select * from menu where type="soupNsalad"')
+    soupNsalad = cur.fetchall()
+
+    result = cur.execute('select * from menu where type="maki"')
+    maki = cur.fetchall()
+
+    result = cur.execute('select * from menu where type="vegmaki"')
+    vegmaki = cur.fetchall()
+
+    result = cur.execute('select * from menu where type="alacarte"')
+    alacarte = cur.fetchall()
+
+    result = cur.execute('select * from menu where type="sashimi"')
+    sashimi = cur.fetchall()
+
+    result = cur.execute('select * from menu where type="maindish"')
+    maindish = cur.fetchall()
+
+    result = cur.execute('select * from menu where type="sushidinner"')
+    sushidinner = cur.fetchall()
+
+    result = cur.execute('select * from menu where type="love"')
+    love = cur.fetchall()
+
+    result = cur.execute('select * from menu where type="bento"')
+    bento = cur.fetchall()
+
+    return render_template('menu.html', optionsArray=optionsArray,options=options, 
+        appetizers=appetizers, lunch=lunch, tray=tray, soupNsalad=soupNsalad, 
+        maki=maki, vegMaki=vegmaki, alacarte=alacarte, sashimi=sashimi,
+        maindish=maindish, sushidinner=sushidinner, love=love, bento=bento)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
